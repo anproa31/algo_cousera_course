294fe85c023c2ef63df99aea92433528f65cd70e
anproa31 <antoeta31@gmail.com>
2023-07-14 09:26:05 +0700

    add last digit fib number

--- Algorithm Toolbox/last_digit_of_fib_number.cpp
+++ Algorithm Toolbox/last_digit_of_fib_number.cpp
@@ -5,8 +5,9 @@ long long findNthFibonaci (long long n) {
     long long arr[n+2];
     arr[0] = 0;
     arr[1] = 1;
-    for(long long i = 2; i <= n; i++) {
-        arr[i] = arr[i-1] + arr[i-2];
+    long long i;
+    for(i = 2; i <= n; i++) {
+        arr[i] = (arr[i-1] + arr[i-2]) % 10;
     }
     return arr[n];
 }
@@ -15,5 +16,5 @@ long long findNthFibonaci (long long n) {
 int main() {
     long long n;
     cin >> n;
-    cout << findNthFibonaci(n);
+    cout << findNthFibonaci(n) << endl;
 }

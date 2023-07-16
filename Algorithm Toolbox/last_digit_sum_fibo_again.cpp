47cd3e646fd8ac874946caeccf3e3d3a5ae51d63
anproa31 <antoeta31@gmail.com>
2023-07-16 15:03:53 +0700

    fix test case last_digit_sum_again

--- Algorithm Toolbox/last_digit_sum_fibo_again.cpp
+++ Algorithm Toolbox/last_digit_sum_fibo_again.cpp
@@ -3,28 +3,35 @@
 #include <iostream>
 using namespace std;
 
-long long calc_fib(long long n) {
+long long calc_fib(long long n)
+{
 
-    n = (n+2)%60;
-    int fib[n+1];
-    fib[0]=0;
-    fib[1]=1;
+    n = (n + 2) % 60;
+    int fib[n + 1];
+    fib[0] = 0;
+    fib[1] = 1;
     int res = 1;
-    for(int i = 2; i<=n;i++){
-        fib[i] = (fib[i-1]%10 + fib[i-2]%10)%10;
+    for (int i = 2; i <= n; i++)
+    {
+        fib[i] = (fib[i - 1] % 10 + fib[i - 2] % 10) % 10;
     }
-    if(fib[n] == 0){
+    if (fib[n] == 0)
+    {
         return 9;
     }
-    return (fib[n]%10-1);
+    return (fib[n] % 10 - 1);
 }
 
-int main() {
-    long long n = 0;
-    long long m =0;
-    std::cin >> n;
+int main()
+{
+    long long n;
+    long long m;
     std::cin >> m;
-
-    std::cout << (calc_fib(n) - calc_fib(m - 1)) % 10<< '\n';
+    std::cin >> n;
+    if (calc_fib(n) < calc_fib(m - 1)) {
+        cout << 10 + calc_fib(n) - calc_fib(m - 1) << endl;
+    } else {
+        cout << calc_fib(n) - calc_fib(m - 1) << endl;
+    }
     return 0;
 }

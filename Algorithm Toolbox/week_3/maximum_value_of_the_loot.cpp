b0643a51e49bc630edcf1966af3619f0967adf4a
anproa31 <antoeta31@gmail.com>
2023-08-08 13:19:42 +0700

    solution value of the loot

--- Algorithm Toolbox/week_3/maximum_value_of_the_loot.cpp
+++ Algorithm Toolbox/week_3/maximum_value_of_the_loot.cpp
@@ -1,5 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
 using namespace std;
-int main() {
 
+struct Item
+{
+    long long cost;
+    long long weight;
+    double rate;
+};
+
+bool compare(Item i1, Item i2)
+{
+    return i1.rate > i2.rate;
+}
+
+int main()
+{
+    int n;
+    long long W;
+    cin >> n >> W;
+    vector<Item> items(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> items[i].cost >> items[i].weight;
+        items[i].rate = (double)items[i].cost / items[i].weight;
+    }
+    sort(items.begin(), items.end(), compare);
+    double value = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (W == 0)
+        {
+            cout << fixed << setprecision(4) << value << endl;
+            return 0;
+        }
+        double a = min(items[i].weight, W);
+        value += a * items[i].rate;
+        W -= a;
+    }
+    cout << fixed << setprecision(4) << value << endl;
 }
